1ca24733c29c187950599576859b502231522a6a
khai96_ <hvksmr1996@gmail.com>
2017-04-07 20:15:14 +0700

    Run standard --fix

--- index.js
+++ index.js
@@ -1,45 +1,42 @@
 
 ((module) => {
-	'use strict';
+  'use strict'
 
-	var createClassFromSuper = require('simple-class-utils').createClass.super;
-	var {XIterable, Root} = require('x-iterable-base');
+  var createClassFromSuper = require('simple-class-utils').createClass.super
+  var {XIterable, Root} = require('x-iterable-base')
 
-	var {iterator} = Symbol;
+  var {iterator} = Symbol
 
-	class SpreadIterable extends XIterable(Root) {
+  class SpreadIterable extends XIterable(Root) {
+    constructor (iterable) {
+      super()
+      this.iterable = iterable
+    }
 
-		constructor(iterable) {
-			super();
-			this.iterable = iterable;
-		}
+    * [iterator] () {
+      for (let element of this.iterable) {
+        yield * element
+      }
+    }
 
-		* [iterator]() {
-			for (let element of this.iterable) {
-				yield * element;
-			}
-		}
+    static many (...args) {
+      return new SpreadIterable(args.map((iterable) => new SpreadIterable(iterable)))
+    }
 
-		static many(...args) {
-			return new SpreadIterable(args.map((iterable) => new SpreadIterable(iterable)));
-		}
+    static times (...args) {
+      return SpreadIterable.deep(...args)
+    }
 
-		static times(...args) {
-			return SpreadIterable.deep(...args);
-		}
-
-		static deep(iterable, level) {
-			if (!isFinite(level) || level < 0) {
-				throw new TypeError(`${level} must be an unsigned number`);
-			}
-			for (iterable = new XIterable.Yield(iterable), level = parseInt(level); level; --level) {
-				iterable = new SpreadIterable(iterable);
-			}
-			return iterable;
-		}
-
+    static deep (iterable, level) {
+      if (!isFinite(level) || level < 0) {
+        throw new TypeError(`${level} must be an unsigned number`)
+      }
+      for (iterable = new XIterable.Yield(iterable), level = parseInt(level); level; --level) {
+        iterable = new SpreadIterable(iterable)
+      }
+      return iterable
+    }
 	}
 
-	module.exports = SpreadIterable;
-
-})(module);
+  module.exports = SpreadIterable
+})(module)
