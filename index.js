fd96c70986321247a3c79467e8fb4acc506462b0
khai96_ <hvksmr1996@gmail.com>
2017-04-07 20:52:49 +0700

    No wrapper

--- index.js
+++ index.js
@@ -1,41 +1,38 @@
+'use strict'
 
-((module) => {
-  'use strict'
+var {XIterable, Root} = require('x-iterable-base')
 
-  var {XIterable, Root} = require('x-iterable-base')
+var {iterator} = Symbol
 
-  var {iterator} = Symbol
+class SpreadIterable extends XIterable(Root) {
+  constructor (iterable) {
+    super()
+    this.iterable = iterable
+  }
 
-  class SpreadIterable extends XIterable(Root) {
-    constructor (iterable) {
-      super()
-      this.iterable = iterable
+  * [iterator] () {
+    for (let element of this.iterable) {
+      yield * element
     }
+  }
 
-    * [iterator] () {
-      for (let element of this.iterable) {
-        yield * element
-      }
+  static many (...args) {
+    return new SpreadIterable(args.map((iterable) => new SpreadIterable(iterable)))
+  }
+
+  static times (...args) {
+    return SpreadIterable.deep(...args)
+  }
+
+  static deep (iterable, level) {
+    if (!isFinite(level) || level < 0) {
+      throw new TypeError(`${level} must be an unsigned number`)
     }
-
-    static many (...args) {
-      return new SpreadIterable(args.map((iterable) => new SpreadIterable(iterable)))
+    for (iterable = new XIterable.Yield(iterable), level = parseInt(level); level; --level) {
+      iterable = new SpreadIterable(iterable)
     }
+    return iterable
+  }
+}
 
-    static times (...args) {
-      return SpreadIterable.deep(...args)
-    }
-
-    static deep (iterable, level) {
-      if (!isFinite(level) || level < 0) {
-        throw new TypeError(`${level} must be an unsigned number`)
-      }
-      for (iterable = new XIterable.Yield(iterable), level = parseInt(level); level; --level) {
-        iterable = new SpreadIterable(iterable)
-      }
-      return iterable
-    }
-	}
-
-  module.exports = SpreadIterable
-})(module)
+module.exports = SpreadIterable
